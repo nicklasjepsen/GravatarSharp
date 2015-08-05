5c627a22ba599860d843778037b5bce67ed2c9c9
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2015-08-05 15:10:34 +0200

    Updated gitignore for vs and updated assembly info

--- GravatarSharp.Core/Properties/AssemblyInfo.cs
+++ GravatarSharp.Core/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GravatarSharp.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A very simple API for interacting with Gravatar")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GravatarSharp.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("SystemOut")]
+[assembly: AssemblyProduct("GravatarSharp")]
+[assembly: AssemblyCopyright("Copyright © SystemOut 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
